9a0a8b2811649d6a7febaa2ff733699ba79a1bfc
Warayut Poomiwatracanont <55990608+Lezgend@users.noreply.github.com>
2022-12-20 14:53:15 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 from ota.ota_updater import OTAUpdater
+import ota.secrets as secrets
+import time, network, gc
+
+def connectToWifiAndUpdate(): 
+    time.sleep(1)
+    print('Memory free', gc.mem_free())
+    
+    # Connecting to WiFi
+    sta = network.WLAN(network.STA_IF)
+    if not sta.isconnected():
+        print('connecting to network...')
+        sta.active(True)
+    #sta.connect('your wifi ssid', 'your wifi password')
+        sta.connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
+        while not sta.isconnected():
+            pass
+    print('network config:', sta.ifconfig())
 
 def download_and_install_update_if_available():
     o = OTAUpdater("https://github.com/Lezgend/LoRAWAN-KKU-OTAUpdater/")
@@ -10,11 +27,12 @@ def start():
      # from main.x import YourProject
      # project = YourProject()
      # ...
-    utime.sleep_ms(10000)
+    time.sleep_ms(10000)
     import main.start
 
 
 def boot():
+    connectToWifiAndUpdate()
     download_and_install_update_if_available()
     start()
 
