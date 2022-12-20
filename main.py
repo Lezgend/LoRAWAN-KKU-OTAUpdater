34668f777395f8bb441eac5d211e4e370d7f5999
Warayut Poomiwatracanont <55990608+Lezgend@users.noreply.github.com>
2022-12-20 14:25:25 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,21 @@
+from ota.ota_updater import OTAUpdater
+
+def download_and_install_update_if_available():
+    o = OTAUpdater("https://github.com/Lezgend/LoRAWAN-KKU-OTAUpdater/")
+    o.install_update_if_available_after_boot(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
 
 
-def connectToWifiAndUpdate():
-    import time, machine, network, gc, app.secrets as secrets
-    time.sleep(1)
-    print('Memory free', gc.mem_free())
-
-    from app.ota_updater import OTAUpdater
-
-    sta_if = network.WLAN(network.STA_IF)
-    if not sta_if.isconnected():
-        print('connecting to network...')
-        sta_if.active(True)
-        sta_if.connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
-        while not sta_if.isconnected():
-            pass
-    print('network config:', sta_if.ifconfig())
-    otaUpdater = OTAUpdater('https://github.com/Lezgend/LoRAWAN-KKU-OTAUpdater', main_dir='app', secrets_file="secrets.py")
-    hasUpdated = otaUpdater.install_update_if_available()
-    if hasUpdated:
-        machine.reset()
-    else:
-        del(otaUpdater)
-        gc.collect()
-
-def startApp():
-    import app.start
+def start():
+     # your custom code goes here. Something like this: ...
+     # from main.x import YourProject
+     # project = YourProject()
+     # ...
+    utime.sleep_ms(10000)
+    import main.start
 
 
-connectToWifiAndUpdate()
-startApp()
+def boot():
+    download_and_install_update_if_available()
+    start()
+
+boot()
