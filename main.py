b841d25da6e1cfc6226a153eb24a30e67238585f
Warayut Poomiwatracanont <55990608+Lezgend@users.noreply.github.com>
2022-12-21 14:04:35 +0700

    V1.5

--- main.py
+++ main.py
@@ -1,39 +1,41 @@
 from ota.ota_updater import OTAUpdater
 import ota.secrets as secrets
-import time, network, gc
+import math, random, time, machine, network, gc
 
-def connectToWifiAndUpdate(): 
-    time.sleep(1)
-    print('Memory free', gc.mem_free())
+# Exponential Time Interval
+def time_interval(mean = 5):
+    x = round(-1.0 * mean * math.log(1 - random.random()))
+    print("Time:", x)
+    return x
+
+def connectToWifiAndUpdate():
+    time.sleep(time_interval() + 1)
+    print("Memory free:", gc.mem_free())
     
     # Connecting to WiFi
     sta = network.WLAN(network.STA_IF)
     if not sta.isconnected():
-        print('connecting to network...')
+        print("Connecting to Network...")
         sta.active(True)
-    #sta.connect('your wifi ssid', 'your wifi password')
+        #sta.connect('your wifi ssid', 'your wifi password')
         sta.connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
         while not sta.isconnected():
             pass
-    print('network config:', sta.ifconfig())
+    print("Network Config:", sta.ifconfig())
 
 def download_and_install_update_if_available():
-    o = OTAUpdater("https://github.com/Lezgend/LoRAWAN-KKU-OTAUpdater/")
-    o.install_update_if_available_after_boot(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
-
-
-def start():
-     # your custom code goes here. Something like this: ...
-     # from main.x import YourProject
-     # project = YourProject()
-     # ...
-    time.sleep_ms(10000)
-    import main.start
-
+    otaUpdater = OTAUpdater("https://github.com/Lezgend/LoRAWAN-KKU-OTAUpdater/", main_dir="main")
+    hasUpdated = otaUpdater.install_update_if_available()
+    if hasUpdated:
+        machine.reset()
+    else:
+        del(otaUpdater)
+        gc.collect()
+        # Run a update code
+        import main.start
 
 def boot():
     connectToWifiAndUpdate()
     download_and_install_update_if_available()
-    start()
-
+    
 boot()
